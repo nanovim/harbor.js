8eadc2f107e87024a7d64e796e38b414b1b536c5
Dennis Beier <nanovim@gmail.com>
2020-07-19 09:24:26 +0200

    added absolute positioning for animations

--- js/harbor.js
+++ js/harbor.js
@@ -1,4 +1,4 @@
-function anchorPosition(element,anchor) {
+function anchorPosition(element, anchor) {
     let rect = element.getBoundingClientRect()
     switch (anchor) {
         case "left":
@@ -16,47 +16,81 @@ function anchorPosition(element,anchor) {
     }
 }
 
-function anchorTo(element,anchor,anchorElement,anchor2){
-    let pos1  = anchorPosition(anchorElement,anchor2)
+function anchorTo(element, anchor, anchorElement, anchor2, positioning) {
+    let pos1 = anchorPosition(anchorElement, anchor2)
     let rect = element.getBoundingClientRect()
-    element.style.position = "fixed"
-    let pos = pos1 +"px"
-    switch (anchor) {
-        case "left":
-            element.style.left = pos
-            return 0
-        case "right":
-            pos = pos1 - rect.width
-            element.style.left = pos + "px"
-            return 0
-        case "bottom":
-            pos = pos1 - rect.height
-            element.style.top = pos + "px"
-            return 0
-        case "top":
-            element.style.top = pos
-            return 0
-        case "verticalCenter":
-            pos = pos1 - (rect.height / 2)
-            element.style.top = pos + "px"
-            return 0
-        case "horizontalCenter":
-            pos = pos1 - (rect.width / 2)
-            element.style.left = pos + "px"
-            return 0
+    if(typeof(positioning) === "undefined" || positioning === "fixed"){
+        element.style.position = "fixed"
+        let pos = pos1 + "px"
+        switch (anchor) {
+            case "left":
+                element.style.left = pos
+                return 0
+            case "right":
+                pos = pos1 - rect.width
+                element.style.left = pos + "px"
+                return 0
+            case "bottom":
+                pos = pos1 - rect.height
+                element.style.top = pos + "px"
+                return 0
+            case "top":
+                element.style.top = pos
+                return 0
+            case "verticalCenter":
+                pos = pos1 - (rect.height / 2)
+                element.style.top = pos + "px"
+                return 0
+            case "horizontalCenter":
+                pos = pos1 - (rect.width / 2)
+                element.style.left = pos + "px"
+                return 0
+        }
+    }else if (positioning === "absolute"){
+        element.style.position = "absolute"
+        let pos;
+        switch (anchor) {
+            case "left":
+                element.style.left = "0px"
+                return 0
+            case "right":
+                element.style.right = "0px"
+                return 0
+            case "bottom":
+                element.style.bottom = "0px"
+                return 0
+            case "top":
+                element.style.top = "0px"
+                return 0
+            case "verticalCenter":
+                pos = (rect.height / 2)
+                element.style.top = pos + "px"
+                return 0
+            case "horizontalCenter":
+                pos = (rect.width / 2)
+                element.style.left = pos + "px"
+                return 0
+        }
     }
+
     return 1;
 }
 
-function parseAnchor(anchor){
+function parseAnchor(anchor) {
     anchor = anchor.trim()
     let list = anchor.split(" ")
+    let target = list[0];
+    if(list.length < 3){
+        list.push("fixed")
+    }
     return {
-        target: list[0],
-        anchor: list[1]
+        target: target,
+        anchor: list[1],
+        positioning: list[2]
     }
 }
-function getCustomCssAttribute(element, attribut){
+
+function getCustomCssAttribute(element, attribut) {
     const style = window.getComputedStyle(element)
     const value = style.getPropertyValue(attribut)
     return value
@@ -66,28 +100,28 @@ function render() {
     let list = document.getElementsByClassName("sailingShip");
     let anchors = ["--anchor-left", "--anchor-right", "--anchor-bottom", "--anchor-top",
         "--anchor-verticalCenter", "--anchor-horizontalCenter"];
-    for(let element of list) {
+    for (let element of list) {
         anchors.forEach(function (anchorName) {
-            const anchorstring =  getCustomCssAttribute(element,anchorName)
-            if(anchorstring !== null && anchorstring.length > 0){
+            const anchorstring = getCustomCssAttribute(element, anchorName)
+            if (anchorstring !== null && anchorstring.length > 0) {
                 let anchor = parseAnchor(anchorstring);
                 let elementAnchor = anchorName.split("-")
+                elementAnchor = elementAnchor.filter(x=> x != "")
                 let harbor = null;
-                if(anchor.target === "parent"){
+                if (anchor.target === "parent") {
                     harbor = element.parentNode
                 } else {
                     harbor = document.querySelector(anchor.target)
                 }
-                if(harbor !== null){
-                    anchorTo(element,elementAnchor[elementAnchor.length-1],harbor,anchor.anchor);
+                if (harbor !== null) {
+                    anchorTo(element, elementAnchor[1], harbor, anchor.anchor, anchor.positioning);
                 }
-
             }
-        } )
+        })
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     render()
-    setInterval(render, 200)
-} );
+    setInterval(render, 2000)
+});
